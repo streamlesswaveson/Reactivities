7f9e72c72f8f839d92a3f117209326cf9f8a40e1
streamlesswaveson <streamlesswaveson@gmail.com>
2020-08-26 11:26:21 -0700

    initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        //entity framework convention: property with name 'Id' automatically set as the PK : 
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
