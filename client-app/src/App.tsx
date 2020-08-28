b246f8d563cf3280ddfa0c28127a0405b1b140da
streamlesswaveson <streamlesswaveson@gmail.com>
2020-08-28 05:58:09 -0700

    react - add axios; update componentdidmount to pull value data

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
     state = {
@@ -8,9 +9,14 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            values: [{id: 1, name: 'value 101'}, {id: 2, name: 'value 102'}]
-        })
+        axios.get('http://localhost:5000/api/values')
+            .then((response) =>{ 
+                console.log(response);
+                this.setState({
+                    values: response.data
+                });
+                
+            });
     }
 
     render() {
@@ -20,7 +26,7 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <ul>
                         {this.state.values.map((value: any) => (
-                            <li>{value.name}</li>
+                            <li key={value.id}>{value.name}</li>
                         ))}
                     </ul>
                 </header>
