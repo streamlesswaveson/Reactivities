29f130ca1f8deb8c2d5f21d59b0b712ad7f5a783
streamlesswaveson <streamlesswaveson@gmail.com>
2020-09-01 05:54:34 -0700

    refactor to use IActivity interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,17 +3,22 @@ import {Header, Icon} from 'semantic-ui-react';
 import {List} from 'semantic-ui-react'
 
 import axios from 'axios';
+import { IActivity } from '../models/activity';
 
-class App extends Component {
-    state = {
-        values: []
+interface IState {
+    activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+    readonly state :IState = {
+        activities: []
     }
 
     componentDidMount() {
-        axios.get('http://localhost:5000/api/values')
+        axios.get<IActivity[]>('http://localhost:5000/api/activities')
             .then((response) => {
                 this.setState({
-                    values: response.data
+                    activities: response.data
                 });
 
             });
@@ -27,8 +32,8 @@ class App extends Component {
                     <Header.Content>Reactivities</Header.Content>
                 </Header>
                 <List>
-                    {this.state.values.map((value: any) => (
-                        <List.Item key={value.id}>{value.name}</List.Item>
+                    {this.state.activities.map((activity) => (
+                        <List.Item key={activity.id}>{activity.title}</List.Item>
                     ))}
                 </List>
             </div>
